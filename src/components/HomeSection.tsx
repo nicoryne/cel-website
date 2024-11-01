0bab367f21280712080288dbea7b811da86a8c31
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-11-02 00:31:59 +0800

    [Nicolo] (Home) fixed panel

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -34,6 +34,7 @@ export default function HomeSection({
   const [filterState, setFilterState] = React.useState('All Games');
   const [menuFilterState, toggleMenuFilter] = React.useState(false);
   const dateToday = new Date();
+  const [currentDate, setCurrentDate] = React.useState(dateToday);
 
   const filteredSeries = React.useMemo(() => {
     return seriesList.filter((item) => {
@@ -54,6 +55,30 @@ export default function HomeSection({
     return dateB - dateA;
   });
 
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const sections = sortedDates.map((date) => document.getElementById(date));
+      const scrollY = window.scrollY + window.innerHeight / 16;
+
+      const currentSection = sections.find((section) => {
+        if (section) {
+          const { top, bottom } = section.getBoundingClientRect();
+          return top <= scrollY && bottom >= scrollY;
+        }
+        return false;
+      });
+
+      if (currentSection) {
+        setCurrentDate(new Date(currentSection.id));
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [sortedDates]);
+
   const platformOptions = [
     'All Games',
     ...gamePlatformList.map((platform) => platform.platform_abbrev)
@@ -62,55 +87,57 @@ export default function HomeSection({
   return (
     <>
       {/* Control Panel */}
-      <aside className="flex place-items-center justify-between">
-        {/* Time Group */}
-        <div className="flex flex-col uppercase">
-          {/* Month and Numeric Date */}
-          <time className="text-3xl font-bold md:text-4xl">
-            {dateToday.toLocaleDateString('en-US', {
-              month: 'short',
-              day: 'numeric'
-            })}
-          </time>
-          {/* 'Today' and Weekday Long */}
-          <time className="text-sm md:text-base">
-            Today&nbsp; | &nbsp;
-            {dateToday.toLocaleDateString('en-US', {
-              weekday: 'long'
-            })}
-          </time>
-        </div>
+      <aside className="fixed left-0 right-0 top-24 z-50 h-24 px-8">
+        <div className="mx-auto flex place-items-center justify-between bg-[var(--background)] py-4 md:w-[1050px]">
+          {/* Time Group */}
+          <div className="flex flex-col uppercase">
+            {/* Month and Numeric Date */}
+            <time className="text-3xl font-bold md:text-4xl">
+              {currentDate.toLocaleDateString('en-US', {
+                month: 'short',
+                day: 'numeric'
+              })}
+            </time>
+            {/* 'Today' and Weekday Long */}
+            <time className="text-sm md:text-base">
+              {currentDate.toLocaleDateString('en-US', {
+                weekday: 'long'
+              })}
+            </time>
+          </div>
 
-        {/* Filter Game Button */}
-        <div className="flex flex-col place-items-center space-y-12">
-          <button
-            className="flex h-10 w-40 items-center justify-center gap-2 rounded-lg border-2 border-neutral-800 hover:bg-neutral-800"
-            onClick={() => toggleMenuFilter(!menuFilterState)}
-          >
-            {filterState}
-            <ChevronDownIcon className="h-4 w-4" />
-          </button>
+          {/* Filter Game Button */}
+          <div className="flex flex-col place-items-center space-y-12">
+            <button
+              className="flex h-10 w-40 items-center justify-center gap-2 rounded-lg border-2 border-neutral-800 hover:bg-neutral-800"
+              onClick={() => toggleMenuFilter(!menuFilterState)}
+            >
+              {filterState}
+              <ChevronDownIcon className="h-4 w-4" />
+            </button>
 
-          {menuFilterState && (
-            <div className="absolute flex flex-col text-center">
-              {platformOptions.map((abbrev) => (
-                <button
-                  className="flex h-10 w-40 items-center justify-center gap-2 border-b-2 border-neutral-800 bg-neutral-900 p-4 hover:bg-neutral-800"
-                  key={abbrev}
-                  onClick={() => {
-                    setFilterState(abbrev), toggleMenuFilter(!menuFilterState);
-                  }}
-                >
-                  {abbrev}
-                </button>
-              ))}
-            </div>
-          )}
+            {menuFilterState && (
+              <div className="absolute flex flex-col text-center">
+                {platformOptions.map((abbrev) => (
+                  <button
+                    className="flex h-10 w-40 items-center justify-center gap-2 border-b-2 border-neutral-800 bg-neutral-900 p-4 hover:bg-neutral-800"
+                    key={abbrev}
+                    onClick={() => {
+                      setFilterState(abbrev),
+                        toggleMenuFilter(!menuFilterState);
+                    }}
+                  >
+                    {abbrev}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </aside>
 
       {/* Series Section */}
-      <div className="mt-12 space-y-16">
+      <div className="mt-24 min-h-[90vh] space-y-16 overflow-y-auto">
         {sortedDates.map((date) => (
           <section id={date} key={date} className="grid items-center">
             <h1 className="col-span-1 text-2xl font-bold uppercase md:col-span-2">
