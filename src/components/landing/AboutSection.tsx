c36d84ef5fdad66343383b2e39a4bafe1b5e6a11
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2024-12-29 12:45:12 +0800

    feat: improved landing

--- src/components/landing/AboutSection.tsx
+++ src/components/landing/AboutSection.tsx
@@ -1,40 +1,176 @@
 'use client';
-import AboutCarousel from '@/components/landing/AboutCarousel';
+import about_1 from '@/../../public/images/about_1.webp';
+import about_2 from '@/../../public/images/about_2.webp';
+import about_3 from '@/../../public/images/about_3.webp';
+import about_4 from '@/../../public/images/about_4.webp';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+const statInformation = [
+  { heading: '10', desc: 'Participating Schools' },
+  { heading: '100+', desc: 'Student Volunteers' },
+  { heading: '120+', desc: 'Student Athletes' },
+  { heading: '25K+', desc: ' Followers' },
+  { heading: '2M+', desc: 'Reach' }
+];
 
 export default function AboutSection() {
   return (
     <section
       aria-labelledby="about-heading"
       id="about"
-      className="min-h-screen bg-[var(--cel-red)]"
+      className="min-h-screen bg-white"
     >
       {/* Content Section */}
-      <div className="flex w-full flex-col items-center justify-center space-y-16 p-6 text-center md:p-12 lg:p-24">
+      <div>
         {/* Header */}
-        <header className="mb-6">
-          <h1
-            id="about-heading"
-            className="border-b-2 border-[var(--text-light)] py-4 text-5xl font-bold text-[var(--text-light)]"
-          >
-            About Us
+        <header
+          id="about-heading"
+          className="mx-auto flex flex-col place-items-center gap-4 p-4 text-center"
+        >
+          <h1 className="bg-gradient-to-r from-[var(--cel-blue)] to-[var(--cel-red)] bg-clip-text text-6xl font-bold uppercase text-transparent">
+            We Forge Legends.
           </h1>
-        </header>
-        {/* End of Header */}
-
-        {/* Article */}
-        <article className="mb-6 max-w-xl text-justify text-lg text-[var(--text-light)]">
-          <p>
-            Welcome to the CESAFI Esports League, the den of the best esports
-            student athletes in Cebu! We bring together the brightest and most
-            dedicated players to compete at the highest level, fostering a
-            community of passion, skill, and sportsmanship. Join us as we
-            celebrate the competitive spirit of esports and showcase the future
-            stars of the gaming world in Cebu.
+          <p className="text-wrap text-neutral-500">
+            CESAFI Esports League (CEL) is the biggest student-oriented esports
+            league in Cebu City, fostering students' talents in-game.
           </p>
-        </article>
-        {/* End of Article */}
+        </header>
 
-        {/* End of Footer */}
+        {/* Stat Information */}
+        <div>
+          <ul className="flex flex-wrap justify-evenly p-8">
+            {statInformation.map((info, index) => (
+              <li
+                key={index}
+                className="flex h-auto w-40 flex-col gap-1 p-4 text-center"
+              >
+                <h3 className="text-3xl font-bold text-neutral-800 md:text-5xl">
+                  {info.heading}
+                </h3>
+                <span className="md:text-md text-xs text-neutral-400">
+                  {info.desc}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Pictures */}
+        <div className="flex flex-col">
+          {/* About 1 */}
+          <div className="grid md:grid-cols-2">
+            <div className="relative">
+              <Image
+                src={about_1}
+                alt="CEL Preseason 3 Finale Audience"
+                className="object-contain"
+              />
+            </div>
+            <div className="flex flex-col justify-center gap-4 bg-[var(--background)] p-16">
+              <h1 className="text-xl font-bold uppercase md:text-4xl">
+                Humble Beginnings
+              </h1>
+              <p className="text-xs font-light md:text-base">
+                Etiam aliquam risus non ultricies posuere. Ut eu turpis nec
+                massa viverra posuere ut vel felis. Donec tincidunt metus nec
+                tempus accumsan. Ut ultrices consequat aliquet. Nunc rhoncus,
+                urna quis luctus condimentum, risus lectus fringilla justo, sit
+                amet iaculis ante sapien vitae leo. Mauris scelerisque feugiat
+                neque, quis hendrerit libero efficitur vitae. Aliquam erat
+                volutpat. Pellentesque in magna lorem. Nulla euismod nulla id
+                maximus efficitur. Suspendisse vel bibendum neque, a malesuada
+                velit. Integer tempor lectus et massa volutpat, in aliquet neque
+                euismod. Vestibulum mollis lacus vel ipsum viverra, eu tristique
+                sem sollicitudin. Nunc congue congue velit, a dignissim leo
+                consequat in. In mollis tortor commodo lectus fermentum
+                faucibus.
+              </p>
+            </div>
+          </div>
+
+          {/* About 2 */}
+          <div className="grid md:grid-cols-2">
+            <div className="relative md:order-2">
+              <Image
+                src={about_2}
+                alt="CEL Preseason 3 Finale Production"
+                className="object-contain"
+              />
+            </div>
+            <div className="col-span-1 flex flex-col justify-center gap-4 bg-[var(--background)] p-16">
+              <h1 className="text-xl font-bold uppercase md:text-4xl">
+                Into the Forge
+              </h1>
+              <p className="text-xs font-light md:text-base">
+                Etiam aliquam risus non ultricies posuere. Ut eu turpis nec
+                massa viverra posuere ut vel felis. Donec tincidunt metus nec
+                tempus accumsan. Ut ultrices consequat aliquet. Nunc rhoncus,
+                urna quis luctus condimentum, risus lectus fringilla justo, sit
+                amet iaculis ante sapien vitae leo. Mauris scelerisque feugiat
+                neque, quis hendrerit libero efficitur vitae. Aliquam erat
+                volutpat. Pellentesque in magna lorem. Nulla euismod nulla id
+                maximus efficitur. Suspendisse vel bibendum neque, a malesuada
+                velit. Integer tempor lectus et massa volutpat, in aliquet neque
+                euismod. Vestibulum mollis lacus vel ipsum viverra, eu tristique
+                sem sollicitudin. Nunc congue congue velit, a dignissim leo
+                consequat in. In mollis tortor commodo lectus fermentum
+                faucibus.
+              </p>
+            </div>
+          </div>
+
+          {/* About 3 */}
+          <div className="grid md:grid-cols-2">
+            <div className="relative">
+              <Image
+                src={about_3}
+                alt="CEL Preseason 3 Finale Trophies"
+                className="object-contain"
+              />
+            </div>
+            <div className="col-span-1 flex flex-col justify-center gap-4 bg-[var(--background)] p-16">
+              <h1 className="text-xl font-bold uppercase md:text-4xl">
+                Burning with Passion
+              </h1>
+              <p className="text-xs font-light md:text-base">
+                Etiam aliquam risus non ultricies posuere. Ut eu turpis nec
+                massa viverra posuere ut vel felis. Donec tincidunt metus nec
+                tempus accumsan. Ut ultrices consequat aliquet. Nunc rhoncus,
+                urna quis luctus condimentum, risus lectus fringilla justo, sit
+                amet iaculis ante sapien vitae leo. Mauris scelerisque feugiat
+                neque, quis hendrerit libero efficitur vitae. Aliquam erat
+                volutpat. Pellentesque in magna lorem. Nulla euismod nulla id
+                maximus efficitur. Suspendisse vel bibendum neque, a malesuada
+                velit. Integer tempor lectus et massa volutpat, in aliquet neque
+                euismod. Vestibulum mollis lacus vel ipsum viverra, eu tristique
+                sem sollicitudin. Nunc congue congue velit, a dignissim leo
+                consequat in. In mollis tortor commodo lectus fermentum
+                faucibus.
+              </p>
+            </div>
+          </div>
+
+          {/* Join Us */}
+          <div className="relative h-64 bg-[var(--background)] md:h-96">
+            {/* Background Image */}
+            <Image
+              src={about_4}
+              alt="CEL Preseason 3 Finale Trophies"
+              className="absolute inset-0 h-full w-full object-cover opacity-60"
+            />
+
+            {/* Content Overlay */}
+            <div className="relative z-10 flex h-full items-center justify-center">
+              <motion.button
+                className="text-4xl font-extrabold uppercase"
+                whileHover={{ scale: 1.04 }}
+              >
+                Join The Team
+              </motion.button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/landing/ContactSection.tsx
+++ src/components/landing/ContactSection.tsx
@@ -32,113 +32,102 @@ export default function ContactSection() {
     <section
       aria-labelledby="contact-heading"
       id="contact"
-      className="bg-white"
+      className="bg-[var(--background)] p-8"
     >
       {/* Content Section */}
-      <div className="flex w-full flex-col items-center justify-center space-y-16 p-6 text-center md:p-12 lg:p-24">
+      <div className="flex w-full flex-col items-center justify-center gap-12 p-6 text-center md:flex-row">
         {/* Header */}
-        <header className="mb-6">
+        <header className="w-80 text-left">
           <h1
             id="contact-heading"
-            className="border-b-2 border-[var(--cel-red)] py-4 text-5xl font-bold text-[var(--cel-red)]"
+            className="text-lg font-bold text-white md:text-3xl"
           >
             Contact Us
           </h1>
+          <p className="text-xs font-light">
+            Have any burning questions? Feel free to contact us through our
+            email
+          </p>
         </header>
         {/* End of Header */}
 
         {/* Form */}
-        <form className="mb-4 mt-8 grid gap-2 space-y-4 rounded-lg bg-[var(--cel-navy)] p-8 shadow-lg md:grid-cols-2">
-          {/* First Name Group */}
-          <div className="col-span-2 flex flex-col justify-end space-y-2 md:col-span-1">
-            <div className="flex flex-col items-start">
-              <label
-                htmlFor="fname"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
+        <form className="flex flex-col gap-6">
+          <div className="flex gap-8">
+            {/* First Name */}
+            <div className="flex flex-col gap-2 text-left">
+              <label htmlFor="firstName" className="text-xs">
                 First Name
               </label>
               <input
-                name="fname"
-                id="fname"
-                placeholder="Junior"
-                className="text-md flex-1 rounded-md border-none bg-white p-2 text-[var(--text-dark)] outline-none ring-0 focus:ring-0"
-                type="name"
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="Juan"
+                className="text-xs text-neutral-800"
+                autoComplete="given-name"
+                value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
-          </div>
 
-          {/* Last Name Group */}
-          <div className="col-span-2 flex flex-col justify-end space-y-2 md:col-span-1">
-            <div className="flex flex-col items-start">
-              <label
-                htmlFor="lname"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
+            {/* Last Name */}
+            <div className="flex flex-col gap-2 text-left">
+              <label htmlFor="lastName" className="text-xs">
                 Last Name
               </label>
               <input
-                name="lname"
-                id="lname"
-                placeholder="Marlon"
-                className="text-md flex-1 rounded-md border-none bg-white p-2 text-[var(--text-dark)] outline-none ring-0 focus:ring-0 md:indent-1"
-                type="name"
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="Dela Cruz"
+                className="text-xs text-neutral-800"
+                autoComplete="family-name"
+                value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
             </div>
           </div>
 
-          {/* Email Address Group */}
-          <div className="col-span-2 flex flex-col justify-end space-y-2">
-            <div className="flex flex-col items-start space-y-2">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Email Address
-              </label>
-              <input
-                name="email"
-                id="email"
-                placeholder="example@gmail.com"
-                className="text-md w-full flex-1 rounded-md border-none bg-white p-2 indent-1 text-[var(--text-dark)] outline-none ring-0 focus:ring-0"
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {errorEmail && (
-                <p className="text-xs font-semibold text-red-400">
-                  {errorEmail}
-                </p>
-              )}
-            </div>
+          {/* Email */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="email" className="text-xs">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@gmail.com"
+              className="text-xs text-neutral-800"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
-          {/* Message Group */}
-          <div className="col-span-2 flex flex-col justify-end space-y-2">
-            <div className="flex flex-col items-start">
-              <label
-                htmlFor="message"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Message
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                placeholder="Hello, I'm writing about..."
-                className="text-md w-full flex-1 rounded-md border-none bg-white text-[var(--text-dark)] outline-none ring-0 focus:ring-0"
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </div>
+          {/* Message */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="message" className="text-xs">
+              Message
+            </label>
+            <textarea
+              rows={4}
+              id="message"
+              name="message"
+              placeholder="Hello! I am writing about.."
+              className="text-xs text-neutral-800"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </div>
 
-          {/* Submit Button */}
-          <div className="col-span-2">
+          <div className="w-full">
             <motion.button
-              className="w-full rounded-md border-none bg-[var(--cel-red)] px-8 py-3 text-center text-lg font-bold text-white"
+              type="button"
+              className="w-full bg-[var(--cel-red)] py-1"
               whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
             >
               Submit
             </motion.button>
