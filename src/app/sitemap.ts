439a65e0daf12e487a7afae07c992e98bae4ba13
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-01-03 09:47:02 +0800

    updated sitemap w/ hosting

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,28 +3,16 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://cesafiesportsleague.vercel.app',
+            url: 'https://cesafiesportsleague.com',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1
         },
         {
-            url: 'https://cesafiesports.vercel.app',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1
-        },
-        {
-            url: 'https://cesafiesportsleague.vercel.app/schedule',
+            url: 'https://cesafiesportsleague.com/schedule',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.8
         },
-        {
-            url: 'https://cesafiesports.vercel.app/schedule',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8
-        }
     ]
 }
