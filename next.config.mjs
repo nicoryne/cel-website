23eed1408c507bdf9a4d8e8c6eccf7de9e986f96
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-03-16 11:13:41 +0800

    disabled image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['uqulenyafyepinfweagp.supabase.co'],
-    unoptimized: false
+    unoptimized: true
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false, crypto: false };
